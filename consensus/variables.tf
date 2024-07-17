dec890d4a3bda139182804556a7ca5fc0aedf367
ramin <raminkeene@gmail.com>
2024-07-17 11:33:17 +0100

    start to set up consensus nodes

---
+++ consensus/variables.tf
@@ -0,0 +1,43 @@
+variable "scaleway_access_key" {}
+
+variable "scaleway_secret_key" {}
+
+variable "scaleway_organization_id" {}
+
+variable "scaleway_project_id" {}
+
+variable "default_region" {
+  default = "nl-ams-3"
+}
+
+// add a new region for each bridge we want to launch
+variable "regions" {
+  type = list(string)
+  default = [
+    "fr-par-2",
+    "nl-ams-3",
+    "pl-waw-3",
+  ]
+}
+
+variable "volume_size" {
+  type        = number
+  default     = 8000
+  description = "the size of the bridge's volume in GB"
+}
+
+variable "celestia_network" {
+  type        = string
+  default     = "mocha"
+  description = "the celestia p2p.network to connect to"
+}
+
+variable "core_ip" {
+  type        = string
+  description = "the celestia p2p.network to connect to"
+}
+
+variable "metrics_endpoint" {
+  type        = string
+  description = "the metrics endpoint to use"
+}
