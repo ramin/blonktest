526de4e07b0f70eb9b636225913541d20a224d5a
ramin <raminkeene@gmail.com>
2024-07-30 14:47:00 +0100

    provider

---
+++ da/provider.tf
@@ -0,0 +1,6 @@
+provider "scaleway" {
+  access_key      = var.scaleway_access_key
+  secret_key      = var.scaleway_secret_key
+  organization_id = var.scaleway_organization_id
+  project_id      = var.scaleway_project_id
+}
