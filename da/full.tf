7f66ab9ce5d63cd85b66bd2be027b30322d0ca09
ramin <raminkeene@gmail.com>
2024-07-31 15:29:35 +0100

    remove test connection node

--- da/full.tf
+++ da/full.tf
@@ -25,28 +25,3 @@ locals {
 #     })
 #   }
 # }
-
-
-module "oleg-connection-test" {
-  source      = "../modules/scaleway/instance"
-  name        = "blonks-test-oleg-connection-test"
-  size        = "small"
-  tags        = ["da", "blonks", "full"]
-  region      = "nl-ams-3"
-  volume_size = 400
-
-  # provisioning
-  user_data = {
-    cloud-init = templatefile("${path.module}/../templates/cloud-init/da.yml", {
-      celestia_custom  = local.celestia_custom
-      core_ip          = var.core_ip
-      metrics_endpoint = var.metrics_endpoint
-      volume_size      = var.volume_size
-      node_type        = "full"
-      run_binary       = var.fast_binary_name
-      fast_binary_name = var.fast_binary_name
-      fast_binary_url  = var.fast_binary_url
-      additional_flags = ""
-    })
-  }
-}
